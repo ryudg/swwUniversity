3ba6cdd72efa43d3fe4402eb38eccb795b9e80b3
ryudg <103430498+ryudg@users.noreply.github.com>
2022-11-18 16:16:59 +0900

    Add files via upload

---
+++ js/sub1-2.js
@@ -0,0 +1,13 @@
+
+ $(document).ready(function(){
+    $('.slides').bxSlider({
+        mode: 'horizontal',
+        moveSlides: 1,
+        slideMargin: 1,
+        infiniteLoop: true,
+        slideWidth: 660,
+        minSlides: 3,
+        maxSlides: 3,
+        speed: 800,
+            });
+        });
